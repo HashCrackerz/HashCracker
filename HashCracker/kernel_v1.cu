d112d8b401e419eac0cc0ad0233b600f4804b24c
itsjustwhitee <matteofontolan215@gmail.com>
2026-01-28 16:45:39 +0100

    corretti argomenti di invocazione (incluso block_size)

--- HashCracker/kernel_v1.cu
+++ HashCracker/kernel_v1.cu
@@ -29,34 +29,43 @@ __constant__ char d_charSet[MAX_CHARSET_LENGTH];
 
 int main(int argc, char** argv)
 {
-    /*invocazione: ./kernel [password_in_chiaro] [min_len] [max_len] [file_charset] */
+    /*invocazione: ./kernel <block_size> <password_in_chiaro> <min_len> <max_len> <file_charset>  */
 
     //char charSet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#-.\0"; // 67 caratteri
     //char secret_password[] = "qwerty";
 
     char* charSet, * secret_password;
     int min_test_len, max_test_len;
-    bool dizionario = false;
+    int blockSize;
 
     /* --- CONTROLLO ARGOMENTI DI INVOCAZIONE --- */
-    if (argc != 5) {
+    if (argc != 6) {
         printf("Usage: %s <password_in_chiaro> <min_len> <max_len> <file_charset> \n", argv[0]);
         return 1;
     }
-    secret_password = argv[1];
+    secret_password = argv[2];
 
-    if (!safe_atoi(argv[2], &min_test_len))
+    if (!safe_atoi(argv[1], &blockSize))
+    {
+        perror("Errore nella conversione di min_test_len");
+        exit(1);
+        if (blockSize % 32 != 0)
+        {
+            perror("Warning... block_size dovrebbe essere multiplo di 32");
+        }
+    }
+    if (!safe_atoi(argv[3], &min_test_len))
     {
         perror("Errore nella conversione di min_test_len");
         exit(1);
     }
-    if (!safe_atoi(argv[3], &max_test_len))
+    if (!safe_atoi(argv[4], &max_test_len))
     {
         perror("Errore nella conversione di max_test_len");
         exit(1);
     }
 
-    charSet = leggiCharSet(argv[4]);
+    charSet = leggiCharSet(argv[5]);
     int charSetLen = strlen(charSet);
 
     printf("%s Starting...\n", argv[0]);
